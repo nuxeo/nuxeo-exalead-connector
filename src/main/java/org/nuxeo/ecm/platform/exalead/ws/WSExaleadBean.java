d12a854c8b7c1b193f995d03ff53030e877216b7
Thierry Delprat <tdelprat@nuxeo.com>
2011-01-19 17:45:42 +0100

    Add filtering on lifecycle

--- src/main/java/org/nuxeo/ecm/platform/exalead/ws/WSExaleadBean.java
+++ src/main/java/org/nuxeo/ecm/platform/exalead/ws/WSExaleadBean.java
@@ -1,5 +1,6 @@
 package org.nuxeo.ecm.platform.exalead.ws;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -11,15 +12,19 @@ import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 
 import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DataModel;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.IterableQueryResult;
 import org.nuxeo.ecm.core.api.impl.LifeCycleFilter;
+import org.nuxeo.ecm.core.query.sql.NXQL;
 import org.nuxeo.ecm.platform.api.ws.DocumentBlob;
 import org.nuxeo.ecm.platform.api.ws.DocumentDescriptor;
 import org.nuxeo.ecm.platform.api.ws.session.WSRemotingSession;
 import org.nuxeo.ecm.platform.exalead.ws.api.WSExalead;
+import org.nuxeo.ecm.platform.indexing.gateway.ws.UUIDPage;
 import org.nuxeo.ecm.platform.indexing.gateway.ws.WSIndexingGatewayBean;
 
 @WebService(name = "WSExaleadInterface", serviceName = "WSExaleadService")
@@ -46,6 +51,69 @@ public class WSExaleadBean extends WSIndexingGatewayBean implements WSExalead {
             docs[i++] = new DocumentDescriptor(doc);
         }
         return docs;
+    }
+
+    @Override
+    @WebMethod
+    public UUIDPage getRecursiveChildrenUUIDsByPage(@WebParam(name = "sessionId") String sid, @WebParam(name = "uuid") String uuid , @WebParam(name = "page") int page, @WebParam(name = "pageSize") int pageSize) throws ClientException {
+
+        CoreSession session = initSession(sid).getDocumentManager();
+
+        List<String> uuids = new ArrayList<String>();
+        IdRef parentRef = new IdRef(uuid);
+        DocumentModel parent = session.getDocument(parentRef);
+        String path = parent.getPathAsString();
+
+        String query = "select ecm:uuid from Document where ecm:path startswith '" + path + " AND ecm:currentLifeCycleState != 'deleted' order by ecm:uuid";
+
+        IterableQueryResult result = session.queryAndFetch(query, "NXQL");
+        boolean hasMore = false;
+        try {
+            if (page>1) {
+                int skip=(page-1)*pageSize;
+                result.skipTo(skip);
+            }
+
+            for (Map<String, Serializable> record:result) {
+                uuids.add((String)record.get(NXQL.ECM_UUID));
+                if (uuids.size()==pageSize) {
+                    hasMore=true;
+                    break;
+                }
+            }
+        }
+        finally {
+            result.close();
+        }
+        return new UUIDPage(uuids.toArray(new String[uuids.size()]),page,hasMore) ;
+    }
+
+
+    @Override
+    @WebMethod
+    public String[] getRecursiveChildrenUUIDs(@WebParam(name = "sessionId") String sid, @WebParam(name = "uuid") String uuid ) throws ClientException {
+
+        CoreSession session = initSession(sid).getDocumentManager();
+
+        List<String> uuids = new ArrayList<String>();
+        IdRef parentRef = new IdRef(uuid);
+        DocumentModel parent = session.getDocument(parentRef);
+        String path = parent.getPathAsString();
+
+        String query = "select ecm:uuid from Document where ecm:path startswith '" + path + "'  AND ecm:currentLifeCycleState != 'deleted' order by ecm:uuid";
+
+        IterableQueryResult result = session.queryAndFetch(query, "NXQL");
+
+        try {
+            for (Map<String, Serializable> record:result) {
+                uuids.add((String)record.get(NXQL.ECM_UUID));
+            }
+        }
+        finally {
+            result.close();
+        }
+
+        return uuids.toArray(new String[uuids.size()]);
     }
 
 }
